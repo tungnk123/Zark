85b4bf2ea0586d3e65205fa54db812bb97896344
tungnk123 <doanthanhtungnk123@gmail.com>
2025-03-31 22:32:57 +0700

    Add basic ui LoginScreen

--- app/src/main/java/com/tungnk123/zark/ui/login/LoginScreen.kt
+++ app/src/main/java/com/tungnk123/zark/ui/login/LoginScreen.kt
@@ -1,15 +1,28 @@
 package com.tungnk123.zark.ui.login
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.tungnk123.zark.ui.navigation.NavigationBarMetadataItem
+import com.tungnk123.zark.R
 
 @Composable
 fun LoginScreen(
@@ -17,18 +30,46 @@ fun LoginScreen(
     modifier: Modifier = Modifier,
     loginViewModel: LoginViewModel = hiltViewModel()
 ) {
-    Scaffold(
-        modifier = modifier
-    ) { contentPaddings ->
-        Column(
-            modifier = Modifier.padding(contentPaddings)
-        ) {
-            Text(text = "Login screen")
-            Button(onClick = {
-                navController.navigate(NavigationBarMetadataItem.Signin.route.route)
-            }) {
-                Text("Sign in")
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(R.drawable.bg_login),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds
+        )
+        Scaffold(
+            modifier = Modifier.fillMaxSize(), containerColor = Color.Transparent
+        ) { contentPaddings ->
+            Column(
+                modifier = Modifier
+                    .padding(contentPaddings)
+                    .padding(horizontal = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(71.dp))
+                Image(
+                    painter = painterResource(R.drawable.ic_logo),
+                    modifier = Modifier.size(92.dp),
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.height(60.dp))
+                Text(
+                    text = "Chào mừng bạn trở lại",
+                    fontSize = 20.sp,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                )
+                Spacer(modifier = Modifier.height(7.dp))
+                Text(
+                    text = "Đăng nhập vào tài khoản của bạn để tiếp tục cuộc trò chuyện",
+                    fontSize = 13.sp,
+                    color = Color.Black,
+                    textAlign = TextAlign.Center
+                )
             }
         }
     }
 }
+
