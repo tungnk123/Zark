88dc6b41c9bd135c40e275c2b8e170c2e361c0ff
tungnk123 <doanthanhtungnk123@gmail.com>
2025-04-13 15:08:33 +0700

    Add chat service with custom LocalDateTimeSerializer

---
+++ app/src/main/java/com/tungnk123/zark/data/dto/ChatResponse.kt
@@ -0,0 +1,15 @@
+package com.tungnk123.zark.data.dto
+
+import com.tungnk123.zark.data.serializer.LocalDateTimeSerializer
+import kotlinx.serialization.Serializable
+import java.time.LocalDateTime
+
+@Serializable
+data class ChatResponse(
+    val senderId: Int,
+    val receiverIt: Int,
+    val content: String,
+    @Serializable(with = LocalDateTimeSerializer::class)
+    val timeStamp: LocalDateTime,
+    val isRead: Boolean = false,
+)
